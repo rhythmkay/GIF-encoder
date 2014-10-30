2f065a0cdcff6a1cdd2cd4ff39ff99f308a16836
João Bernardo <jb.amaro@hotmail.com>
2014-10-30 14:59:24 +0000

    Add teacher code

---
+++ GIFencoder.h
@@ -0,0 +1,25 @@
+#ifndef _GIFENCODER_H_
+#define _GIFENCODER_H_
+
+#define MAX_COLORS 256
+
+#include "stdio.h"
+
+typedef struct _imageStruct {
+	int width;
+	int height;
+	char *pixels;
+	char *colors;
+	int numColors;
+	char minCodeSize;
+} imageStruct;
+
+imageStruct* GIFEncoder(unsigned char *data, int width, int height);
+void RGB2Indexed(unsigned char *data, imageStruct* image);
+int nextPower2(int n);
+char numBits(int n);
+void GIFEncoderWrite(imageStruct* image, char* outputFile);
+void writeGIFHeader(imageStruct* image, FILE* file);
+void writeImageBlockHeader(imageStruct* image, FILE* file);
+
+#endif
---
+++ GIFmain.c
@@ -0,0 +1,44 @@
+#define STBI_FAILURE_USERMSG true
+
+#include "stb_image.c"
+
+#include "GIFencoder.h"
+
+int main(int argc, char *argv[]) {
+
+	char *inputFile;
+	char *outputFile;
+    int width, height, numComponents;
+    unsigned char *data;
+	imageStruct* image;
+	
+	if (argc > 1)
+		inputFile = argv[1];
+	else
+		inputFile = "portugal.png";
+
+	if (argc > 2)
+		outputFile = argv[2];
+	else
+		outputFile = "portugalOutput.gif";
+	
+	printf("Loading file %s\n", inputFile);
+	printf("Writing to file %s\n", outputFile);
+
+	data = stbi_load(inputFile, &width, &height, &numComponents, STBI_rgb);
+
+	if (!data && stbi_failure_reason()) {
+		printf("Error: %s\n",stbi_failure_reason());
+		return 1;
+	}
+
+	image = GIFEncoder(data, width, height);
+	GIFEncoderWrite(image, outputFile);
+
+	printf("Compressing image...\n");
+
+    stbi_image_free(data);
+	printf("Done.\n");
+
+	return 0;
+}
