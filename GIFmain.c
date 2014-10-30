cfca72dac6e7747b831e75189b4e8ac3d3590e28
João Bernardo <jb.amaro@hotmail.com>
2014-10-30 15:12:07 +0000

    Indent and organize code

--- GIFmain.c
+++ GIFmain.c
@@ -6,39 +6,39 @@
 
 int main(int argc, char *argv[]) {
 
-	char *inputFile;
-	char *outputFile;
+    char *inputFile;
+    char *outputFile;
     int width, height, numComponents;
     unsigned char *data;
-	imageStruct* image;
-	
-	if (argc > 1)
-		inputFile = argv[1];
-	else
-		inputFile = "portugal.png";
+    imageStruct* image;
 
-	if (argc > 2)
-		outputFile = argv[2];
-	else
-		outputFile = "portugalOutput.gif";
-	
-	printf("Loading file %s\n", inputFile);
-	printf("Writing to file %s\n", outputFile);
+    if (argc > 1)
+        inputFile = argv[1];
+    else
+        inputFile = "portugal.png";
 
-	data = stbi_load(inputFile, &width, &height, &numComponents, STBI_rgb);
+    if (argc > 2)
+        outputFile = argv[2];
+    else
+        outputFile = "portugalOutput.gif";
 
-	if (!data && stbi_failure_reason()) {
-		printf("Error: %s\n",stbi_failure_reason());
-		return 1;
-	}
+    printf("Loading file %s\n", inputFile);
+    printf("Writing to file %s\n", outputFile);
 
-	image = GIFEncoder(data, width, height);
-	GIFEncoderWrite(image, outputFile);
+    data = stbi_load(inputFile, &width, &height, &numComponents, STBI_rgb);
 
-	printf("Compressing image...\n");
+    if (!data && stbi_failure_reason()) {
+        printf("Error: %s\n",stbi_failure_reason());
+        return 1;
+    }
+
+    image = GIFEncoder(data, width, height);
+    GIFEncoderWrite(image, outputFile);
+
+    printf("Compressing image...\n");
 
     stbi_image_free(data);
-	printf("Done.\n");
+    printf("Done.\n");
 
-	return 0;
+    return 0;
 }
